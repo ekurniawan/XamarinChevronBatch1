127a48451020f477112c69a8ed079f2b7a6508cd
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-06-10 13:20:40 +0700

    menambahkan alert

---
+++ Chapter1/Chapter1/ProgressPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chapter1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProgressPage : ContentPage
+    {
+        public ProgressPage()
+        {
+            InitializeComponent();
+        }
+
+        /*protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await myProgress.ProgressTo(1, 1000, Easing.Linear);
+        }*/
+
+        private async void btnProgress_Clicked(object sender, EventArgs e)
+        {
+            myProgress.Progress = 0.2;
+           
+            await myProgress.ProgressTo(1,10000, Easing.Linear);
+        }
+    }
+}
